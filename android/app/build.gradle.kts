80c16ec7aafb5ca289f4cfb4324420129afa94d5
Niloy <niloy.rolling@gmail.com>
2026-08-02 03:16:43 -0700

    update android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // REQUIRED: Google Services Plugin for Firebase / Google Sign-In
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -15,23 +16,29 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.niloy.demo"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        multiDexEnabled = true
+        ndk {
+            abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a", "x86_64"))
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+          isMinifyEnabled = false    // ✅ Correct Kotlin DSL property
+        isShrinkResources = false  // ✅ Correct Kotlin DSL property
         }
     }
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
 }
 
 kotlin {
